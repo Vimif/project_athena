d85718e49816ce1307e8844624fac4d1d11b14f9
Thomas BOISAUBERT <thomascoca2002@gmail.com>
2025-11-10 00:16:44 +0100

    Initial Commit

---
+++ project_athena/project_athenaApp.swift
@@ -0,0 +1,17 @@
+//
+//  project_athenaApp.swift
+//  project_athena
+//
+//  Created by Thomas Boisaubert on 10/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct project_athenaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
